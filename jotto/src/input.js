f3b405915d9209a28588f804e0903034c76bf546
Daryl <darylwalsh@gmail.com>
2018-10-15 03:13:29 -0400

    updated

--- jotto/src/input.js
+++ jotto/src/input.js
@@ -3,12 +3,25 @@ import { connect } from 'react-redux'
 
 class Input extends Component {
   render() {
-    return (
-      <div>
-        {' '}
-        <button>test</button>
-      </div>
+    const contents = this.props.success ? null : (
+      <form className="form-inline">
+        <input
+          type="text"
+          data-test="input-box"
+          className="mb-2 mx-sm-3"
+          id="word-guess"
+          placeholder="enter guess"
+        />
+        <button
+          data-test="submit-button"
+          type="submit"
+          className="btn btn-primary mb-2"
+        >
+          Submit
+        </button>
+      </form>
     )
+    return <div data-test="component-input">{contents}</div>
   }
 }
 
